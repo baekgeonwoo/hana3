b269e75cebbf843db9da097d4d7397da30ad5230
baekgeonwoo <binch789@naver.com>
2024-01-24 17:42:18 +0900

    24.01.24

---
+++ js/hello.js
@@ -0,0 +1,6 @@
+// primitive(call-by-value) vs object(call-by-reference)
+const userName = "Hong";
+let age; // declare + define(undefined) -> 메모리가 할당됨 값은 undefined
+age = 33;
+console.log("🚀 ~ age:", age);
+console.log(`Hello, World!!, ${userName}!!!`);
